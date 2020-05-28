8b2a868abaf6a6a53bd4d8ebb7fd8472afc01b7a
k4nfr3 <root@rita>
2020-05-28 12:37:09 +0200

    small change

--- rita-python.py
+++ rita-python.py
@@ -3,10 +3,11 @@ from ritadns import get_hostname
 import subprocess
 import sys
 
+RITA_CMD="/usr/local/bin/rita"
 
 def main(argument,db):
   try:
-    result = subprocess.run(["/usr/local/bin/rita" +argument], shell=True, check=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+    result = subprocess.run([RITA_CMD +argument], shell=True, check=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
   except subprocess.CalledProcessError as error:
     sys.exit("Exiting ! called Rita with command : " + str(error.cmd) + " returned error : " + str(error.output.decode('ascii')))
 
@@ -20,13 +21,12 @@ def main(argument,db):
     for j in range(len(column)):
       if column[j].find("Destination IP")!=-1: 
        DstIPColumn=j
-       #print(DstIPColumn)
+
   for line in result.stdout.splitlines()[1:]:  #without header
     finaline=""
     columnanswer=line.decode('utf-8').split(',') #split line csv into cells
     for k in range(len(columnanswer)):
       if k==DstIPColumn: # Does output Column contain "Destination IP"
-        #finaline+=(get_hostname(columnanswer[k],1,False,True,db))+"," 
         finaline+=columnanswer[k]+","+(get_hostname(columnanswer[k],1,False,True,db))+"," 
       else :
         finaline+=(columnanswer[k])+","
@@ -34,8 +34,7 @@ def main(argument,db):
 
 if __name__ == "__main__":
   if len(sys.argv) <= 1:
-    print("Usage " + sys.argv[0] + " show-beacons <db>")
-    sys.exit("Usage " + sys.argv[0] + " show-beacons <db> --min 0.8")
+    sys.exit("Usage " + sys.argv[0] + " show-beacons <db>")
   argument=""
   for i in range(1,len(sys.argv)):
     argument+= " " + sys.argv[i]
